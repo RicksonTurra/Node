7228dd6a9a1c203635a7ab8b590364e25da03191
RIckson <rickynhoct@gmail.com>
2022-07-03 02:17:16 +0300

    add a res.json to app.get with the message Hello json

--- server.js
+++ server.js
@@ -30,6 +30,12 @@ app.get("/", function(req, res) {
 
 app.use("/public", middleFunc);
 
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json"
+  });
+});
+
 
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
