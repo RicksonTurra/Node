fba915dec535e484fcf51a97ec583728c32e170e
RIckson <rickynhoct@gmail.com>
2022-07-10 04:08:24 +0300

    move /now up the code

--- myApp.js
+++ myApp.js
@@ -5,23 +5,6 @@ let app = express();
 
 console.log("Hello World");
 
-app.use(function middleware(req, res, next) {
-    // Do something
-    var string = req.method + " " + req.path + " - " + req.ip;
-    // Call the next function in line:
-    console.log(string)
-    next();
-  });
-
-  
-app.get("/json", (req, res) => {
-    var response = {"message":"Hello json"}
-    if (process.env.MESSAGE_STYLE === "uppercase"){
-        response.message = response.message.toUpperCase();
-        }
-    res.json(response)
-});
-
 app.get(
     "/now",
     (req, res, next) => {
@@ -40,6 +23,25 @@ app.get(
         )
     }
   );
+  
+app.use(function middleware(req, res, next) {
+    // Do something
+    var string = req.method + " " + req.path + " - " + req.ip;
+    // Call the next function in line:
+    console.log(string)
+    next();
+  });
+
+  
+app.get("/json", (req, res) => {
+    var response = {"message":"Hello json"}
+    if (process.env.MESSAGE_STYLE === "uppercase"){
+        response.message = response.message.toUpperCase();
+        }
+    res.json(response)
+});
+
+
 
 
 
