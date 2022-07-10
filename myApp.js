b302d077ae52be712061fd8657831b131114ad11
RIckson <rickynhoct@gmail.com>
2022-07-10 03:08:04 +0300

    middleware

--- myApp.js
+++ myApp.js
@@ -5,7 +5,15 @@ let app = express();
 
 console.log("Hello World");
 
+app.use(function middleware(req, res, next) {
+    // Do something
+    var string = req.method + " " + req.path + " - " + req.ip;
+    // Call the next function in line:
+    console.log(string)
+    next();
+  });
 
+  
 app.get("/json", (req, res) => {
     var response = {"message":"Hello json"}
     if (process.env.MESSAGE_STYLE === "uppercase"){
@@ -14,13 +22,6 @@ app.get("/json", (req, res) => {
     res.json(response)
 });
 
-app.use(function middleware(req, res, next) {
-    // Do something
-    var string = req.method + " " + req.path + " - " + req.ip;
-    // Call the next function in line:
-    console.log(string)
-    next();
-  });
 
 
 
